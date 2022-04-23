580714ca589707a2e055071af066812301657445
leminhkhanh81 <khanhle.t3@gmail.com>
2022-04-23 15:17:12 +0700

    Initial Commit

---
+++ WeSplitSwiftUIBootcamp/WeSplitSwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitSwiftUIBootcampApp.swift
+//  WeSplitSwiftUIBootcamp
+//
+//  Created by Le Minh Khanh on 23/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitSwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
